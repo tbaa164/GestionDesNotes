4c93eb32437f9a51e591fdfb911cc39895c855ce
haasjaalo <ehod@arc.sn>
2024-11-08 12:18:08 +0000

    Ajoutez des fichiers projet.

---
+++ UsingCollectionsDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+
+namespace UsageCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList lstEmployé = new SortedList();
+            lstEmployé.Add("Raci" +
+                "ne", new Employé { Nom = "Racine", 
+                Age= 32 });
+            Employé layssa = new Employé()
+            {
+                Nom = "layssa",
+                Age = 25
+            };
+            lstEmployé.Add(layssa.Nom, layssa);
+            Employé unEmployé = (Employé)lstEmployé["Racine"];
+
+            if (unEmployé != null)
+            {
+                Console.WriteLine($"Nom: {unEmployé.Nom}, Age:" +
+                    $" {unEmployé.Age.ToString()}");
+            }
+
+            Console.ReadLine();
+
+            foreach (DictionaryEntry employé in lstEmployé)
+            {
+                Employé autreEmployé = (Employé)employé.Value;
+                Console.WriteLine($"Nom: {autreEmployé.Nom}, Age: {autreEmployé.Age.ToString()}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
